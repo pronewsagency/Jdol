034b3ab24cbd3458464e88106e5015ec6132ac38
pronewsagency <hussam.alrubaie.2019@gmail.com>
2025-08-13 02:36:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const teamLogos = {
-    "real_madrid": "https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg",
-    "barcelona": "https://upload.wikimedia.org/wikipedia/ar/b/ba/FC_Barcelona_logo.svg",
-    "liverpool": "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
-    "manchester_united": "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
-    "paris_saint_germain": "https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg"
+    "real_madrid": {
+        url: "https://upload.wikimedia.org/wikipedia/en/5/56/Real_Madrid_CF.svg",
+        keywords: ["real madrid", "ريال مدريد", "ريال", "مدريد"]
+    },
+    "barcelona": {
+        url: "https://upload.wikimedia.org/wikipedia/ar/b/ba/FC_Barcelona_logo.svg",
+        keywords: ["barcelona", "برشلونة", "برشا", "برشلونه"]
+    },
+    "liverpool": {
+        url: "https://upload.wikimedia.org/wikipedia/en/0/0c/Liverpool_FC.svg",
+        keywords: ["liverpool", "ليفربول"]
+    },
+    "manchester_united": {
+        url: "https://upload.wikimedia.org/wikipedia/en/7/7a/Manchester_United_FC_crest.svg",
+        keywords: ["manchester united", "مانشستر يونايتد", "مانشستر", "مان يونايتد"]
+    },
+    "paris_saint_germain": {
+        url: "https://upload.wikimedia.org/wikipedia/en/a/a7/Paris_Saint-Germain_F.C..svg",
+        keywords: ["paris saint-germain", "باريس سان جيرمان", "باريس", "p.s.g", "psg"]
+    }
 };
 
 function getTeamLogoUrl(teamName) {
-    const formattedTeamName = teamName.toLowerCase().replace(/ /g, '_');
-    return teamLogos[formattedTeamName] || null;
+    const formattedTeamName = teamName.toLowerCase().trim();
+
+    for (const teamKey in teamLogos) {
+        if (teamLogos[teamKey].keywords.includes(formattedTeamName)) {
+            return teamLogos[teamKey].url;
+        }
+    }
+    return null;
 }
 
 function displayTeamLogo(teamName, imageElementId) {
