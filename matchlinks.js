7fdde53a87252612575d4497ef174865db3b9979
pronewsagency <hussam.alrubaie.2019@gmail.com>
2025-08-13 22:33:03 +0300

    Create matchlinks.js

---
+++ matchlinks.js
@@ -0,0 +1,18 @@
+const matchLinks = {
+  hilal_nassr: {
+    url: 'go:a',
+    keywords: ['الهلال', 'النصر', 'hilal', 'nassr']
+  },
+  real_barca: {
+    url: 'go:b',
+    keywords: ['ريال مدريد', 'برشلونة', 'real', 'barca']
+  },
+  liverpool_united: {
+    url: 'go:c',
+    keywords: ['ليفربول', 'مانشستر يونايتد', 'liverpool', 'united']
+  },
+  egypt_algeria: {
+    url: 'go:d',
+    keywords: ['مصر', 'الجزائر', 'egypt', 'algeria']
+  }
+};
