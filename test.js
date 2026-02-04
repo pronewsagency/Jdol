22f6fa89f0fc6977c07db101cfc08f695060f084
pronewsagency <hussam.alrubaie.2019@gmail.com>
2026-02-04 03:30:44 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,44 +1,92 @@
 {
-  "id": "alsumaria",
-  "name": "السومرية",
-  "logo": "https://example.com/alsumaria.png",
-  "servers": [
+  "channels": [
     {
-      "type": "page",
-      "url": "https://www.elahmad.com/tv/watchtv.php?id=alsumaria",
-      "headers": {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-        "referer": "https://www.elahmad.com/"
-      }
+      "id": "alsumaria",
+      "name": "السومرية",
+      "logo": "https://example.com/alsumaria.png",
+      "servers": [
+        {
+          "type": "page",
+          "url": "https://www.elahmad.com/tv/watchtv.php?id=alsumaria",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "https://www.elahmad.com/"
+          }
+        },
+        {
+          "type": "m3u8",
+          "url": "http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "none"
+          }
+        },
+        {
+          "type": "page",
+          "url": "https://asd.pics/%d9%85%d8%b3%d9%84%d8%b3%d9%84-%d8%a8%d9%8a%d8%aa-%d8%a8%d8%a7%d8%a8%d8%a7-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-1-%d8%a7%d9%84%d8%a7%d9%88%d9%84%d9%89/watch/",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "https://asd.pics/"
+          }
+        },
+        {
+          "type": "mpd",
+          "url": "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-action/8ea6ca1108d03e03da5e61372354f5c8/index.mpd",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "none"
+          },
+          "drm": {
+            "scheme": "widevine",
+            "key_id": "e3ce77324a3d4fa2a913b26cc1976052",
+            "key": "17774f82a3b9e33ea7a149596acbb20f"
+          }
+        }
+      ]
     },
     {
-      "type": "m3u8",
-      "url": "http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8",
-      "headers": {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-        "referer": "none"
-      }
-    },
-    {
-      "type": "page",
-      "url": "https://asd.pics/%d9%85%d8%b3%d9%84%d8%b3%d9%84-%d8%a8%d9%8a%d8%aa-%d8%a8%d8%a7%d8%a8%d8%a7-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-1-%d8%a7%d9%84%d8%a7%d9%88%d9%84%d9%89/watch/",
-      "headers": {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-        "referer": "https://asd.pics/"
-      }
-    },
-    {
-      "type": "mpd",
-      "url": "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-action/8ea6ca1108d03e03da5e61372354f5c8/index.mpd",
-      "headers": {
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-        "referer": "none"
-      },
-      "drm": {
-        "scheme": "widevine",
-        "key_id": "e3ce77324a3d4fa2a913b26cc1976052",
-        "key": "17774f82a3b9e33ea7a149596acbb20f"
-      }
+      "id": "aliraqia",
+      "name": "العراقية",
+      "logo": "https://example.com/aliraqia.png",
+      "servers": [
+        {
+          "type": "page",
+          "url": "https://www.elahmad.com/tv/watchtv.php?id=aliraqia",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "https://www.elahmad.com/"
+          }
+        },
+        {
+          "type": "m3u8",
+          "url": "http://het000.4rouwanda-shop.store/live/918454578001/index.m3u8",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "none"
+          }
+        },
+        {
+          "type": "page",
+          "url": "https://asd.pics/%d9%85%d8%b3%d9%84%d8%b3%d9%84-%d8%a8%d9%8a%d8%aa-%d8%a8%d8%a7%d8%a8%d8%a7-%d8%a7%d9%84%d8%ad%d9%84%d9%82%d8%a9-1-%d8%a7%d9%84%d8%a7%d9%88%d9%84%d9%89/watch/",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "https://asd.pics/"
+          }
+        },
+        {
+          "type": "mpd",
+          "url": "https://shd-gcp-live.edgenextcdn.net/live/bitmovin-mbc-action/8ea6ca1108d03e03da5e61372354f5c8/index.mpd",
+          "headers": {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+            "referer": "none"
+          },
+          "drm": {
+            "scheme": "widevine",
+            "key_id": "e3ce77324a3d4fa2a913b26cc1976052",
+            "key": "17774f82a3b9e33ea7a149596acbb20f"
+          }
+        }
+      ]
     }
   ]
 }
